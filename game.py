21380d47f31524428a95d096a94b2ad383e43810
Smallturtles <topher.parrish@gmail.com>
2021-08-11 16:13:10 -0700

    Basic if/elif/else program

--- game.py
+++ game.py
@@ -6,11 +6,15 @@ action_input = input('Action: ')
 
 if action_input == 'Bed':
     print("You walk over to the bed.")
+
 elif action_input == 'Mirror':
     print("You walk to the mirror.")
+
 elif action_input == 'Chamber pot':
     print("You walk over to the chamber pot.")
+
 elif action_input == 'Bars':
     print("You walk over to the bars.")
+
 else:
     print("Invalid action!")
