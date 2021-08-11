0d3fdc7acb99d97ed6e5f5f99a6fb98565735fe8
Smallturtles <topher.parrish@gmail.com>
2021-08-11 15:27:07 -0700

    Basic if/elif/else program

---
+++ game.py
@@ -0,0 +1,16 @@
+print("Escape from the cell!")
+
+print("you see a Bed, the bars, a Mirror, and a Chamber pot.")
+
+action_input = input('Action: ')
+
+if action_input == 'Bed':
+    print("You walk over to the bed.")
+elif action_input == 'Mirror':
+    print("You walk to the mirror.")
+elif action_input == 'Chamber pot':
+    print("You walk over to the chamber pot.")
+elif action_input == 'Bars':
+    print("You walk over to the bars.")
+else:
+    print("Invalid action!")
