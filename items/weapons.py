84174a91600dfb22361ccf160ad17769484e5177
Smallturtles <topher.parrish@gmail.com>
2021-08-11 18:23:42 -0700

    added 4 weapons (dagger, sword, rock, unarmed)

---
+++ items/weapons.py
@@ -0,0 +1,23 @@
+class dagger:
+    def __init__(self):
+        self.name = "Dagger"
+        self.description = "A small dagger with some rust."
+        self.damage = 10
+
+class sword:
+    def __init__(self):
+        self.name = "Sword"
+        self.description = "A short sword."
+        self.damage = 12
+
+class rock:
+    def __init__(self):
+        self.name = "Rock"
+        self.description = "A fist sized rock, could be used for bludgeoning"
+        self.damage = 5
+
+class Unarmed:
+    def __init__(self):
+        self.name = "Unarmed"
+        self.description = "Just some trusty ol fists"
+        self.damage = 3
--- objects.py
+++
@@ -1,5 +0,0 @@
-class dagger:
-    def __init__(self):
-        self.name = "Dagger"
-        self.description = "A small dagger with some rust."
-        self.damage = 10
