f61bd61c0df594adbeaeb8bc07053aeef17d861e
Smallturtles <topher.parrish582@gmail.com>
2021-09-09 10:33:34 -0700

    added game_text.py, moved most functions to game_text.py, added __init__.py to folder.

--- game.py
+++ game.py
@@ -5,14 +5,12 @@ print("Escape from the cell!")
 print("you see a Bed, the Bars, a Mirror, and a Chamber pot.")
 
 action_input = input('Action: ')
-answer = action_input(">")
 
 if action_input == 'Bed':
     print("You walk over to the bed.")
 
-
-elif "m" in answer:
-    print("you walk over to mirror")
+elif action_input == 'Mirror':
+    print("You walk to the mirror.")
 
 elif action_input == 'Chamber pot':
     print("You walk over to the chamber pot.")
--- game1.py
+++ game1.py
@@ -1,40 +1,51 @@
-intro_message = '''intro_message = It's a cold windy night with no clouds. You walk into a building called
-The Howling Serpent Inn. You sit down and the bartender hands you the normal order, a 
-fine Elven brew called Evermead. You go to sit at a table when you hear a small group 
-of three trying to keep their voices lower than everyone else.
+import items.game_text
 
-intro_q = Why are they keeping so quiet?
-I could try to Listen in on their conversation, but it might be easier to just Disregard them '''
-def Start():
 
-    print("\nIt's a cold windy night with no clouds. You walk into a building called The Howling Serpent Inn.")
-    print("You sit down and the bartender hands you the normal order, a fine Elven brew called Evermead.")
-    print("You go to sit at a table when you hear a small group of three trying to keep their voices lower than everyone else.")
-    print("\nWhy are they keeping so quiet?")
-    print("I could try to Listen in on their conversation, but it might be easier to just Disregard them")
+def start():
+    print(items.game_text.intro_message)
+    print(items.game_text.intro_q)
+
     while True:
-        answer = input("\nWhat should I do?\n>").lower()
-        failed = "Great, now the world has been destroyed because you decided you didn't want to choose one of the options."
-        if answer == "l" or answer == "listen":
+        failed = "Great, now the world has been destroyed because you decided you didn't want to choose one of the " \
+                 "options."
+        if items.game_text.answer == "l" or items.game_text.answer == "listen":
             # if l or listen is typed for answer
             listen()
-        elif answer == "d" or answer == "disregard":
+        elif items.game_text.answer == "d" or items.game_text.answer == "disregard":
             # if d or disregard is typed for answer
-            pass
+            disregard()
         else:
             # calls game_over() function with a "reason" argument
             game_over(failed)
 
-#listen to the group
+
+# listen to the group
+def disregard():
+    print(items.game_text.disregard_text)
+
+    if items.game_text.answer == "f" or items.game_text.answer == "follow":
+        follow()
+
+    elif items.game_text.answer == "s" or items.game_text.answer == "sleep":
+        game_over(items.game_text.sleep)
+
+
 def listen():
-    print(''' \tYou sit at a table right behind the group. You hear some small talk about "The Grays"
- being dug up and urns being stolen from around and outside of the city.
- The Grays?\n\n\tWho would be digging up dead bodies? Maybe some grave robbers?
- But they said the bodies are nowhere to be found.
- Should i step in to get more information? or should i stay quiet and keep listening? ''')
+    print(items.game_text.listen)
+    if items.game_text.answer == "c" or items.game_text.answer == "continue":
+        print("hi")
+    elif items.game_text.answer == "s" or items.game_text.answer == "step in":
+        print("hi")
+    else:
+        print("hi")
+
+
+def follow():
+    print(items.game_text.follow)
 
 
 def game_over(state):
     print(state)
 
-Start()
+
+start()
