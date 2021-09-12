02db50b989da4d21cd07a04bb9b4302eb8702667
jason <mcclujdd@mail.com>
2021-09-11 20:37:37 -0400

    added shortcut options to help

--- items/game_text.py
+++ items/game_text.py
@@ -5,13 +5,13 @@ You go to sit at a table when you hear a small group of three trying to keep the
 intro_q = '''Why are they keeping so quiet? I could try to Listen in on their conversation,but it might be easier to just Disregard them'''
 
 options = {
-'disregard': '''You go to an isolated table in the corner to sit back, relax, and enjoy the beautiful sound of the 
+'disregard (d)': '''You go to an isolated table in the corner to sit back, relax, and enjoy the beautiful sound of the 
     instrument played by the bards on the stage. Out of the corner of your eye you see the small group stand up 
     and walk out.\n\tIt looked like they were in a rush of some sort. Are they going to try to find the person 
     responsible?  I could follow them without them even knowing, but it is getting late in the night and i am a 
     little tired.''',
 
-'sleep': '''As the night goes on you contiue drinking that delicious Evermead. You rent a room upstairs with a nice view of the street. 
+'sleep (s)': '''As the night goes on you contiue drinking that delicious Evermead. You rent a room upstairs with a nice view of the street. 
 You undress and lay down on the bed and almost immediatly fall asleep.
 \nYou wake up to some ear wrenching screeches. You look out the window to see some deathly looking dragon hawks with holes in their wings. 
 As you turn around to put your armor on your door gets busted down by humanoid creature dorned in so much plate that you cant see any skin other than the 
@@ -19,11 +19,11 @@ burning red eyes peering through the helmet. In fact his hands are skeletal! he 
 Ash fills the room and it becomes difficult to breathe, all you can see is their fiery eyes swarming you as your vision goes black.
 \nYou have died''',
 
-'listen': '''You sit at a table right behind the group. You hear some small talk about "The Grays" being dug up and urns being stolen from around and outside of the city.
+'listen (l)': '''You sit at a table right behind the group. You hear some small talk about "The Grays" being dug up and urns being stolen from around and outside of the city.
 The Grays? Who would be digging up dead bodies? Maybe some grave robbers? But they said the bodies are nowhere to be found.
 \nShould i step in to get more information? or should i stay quiet and keep listening?''',
 
-'investigate': f"You look around the room and see the walls and floor. It's pretty basic.",
+'investigate (iv)': f"You look around the room and see the walls and floor. It's pretty basic.",
 
-'follow': '''You pull up your bootstraps and start tailing the individual you seek.'''
+'follow (f)': '''You pull up your bootstraps and start tailing the individual you seek.'''
 }
