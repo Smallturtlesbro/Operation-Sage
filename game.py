8d2562829197a44612fa2803e8de3d9a12a10df8
Smallturtles <topher.parrish582@gmail.com>
2021-09-12 23:44:26 -0700

    added sleep, asks if player wants to restart after death. added investigate function fixed some grammar issues and narration in game_text.py

--- game.py
+++ game.py
@@ -3,7 +3,6 @@ import items.game_text as gt
 
 def start():
     print(gt.intro_message)
-    print(gt.intro_q)
 
 
     while True:
@@ -18,11 +17,14 @@ def start():
         elif answer == "d" or answer == "disregard":
             # if d or disregard is typed for answer
             disregard()
-        elif answer == "exit":
+        elif answer == "s" or answer == "sleep":
+            if sleep(input, gt.intro_message, "") == True:
+                return
+        elif answer == "e" or answer == "exit":
             if exit_game(input, "Goodbye.", "") == True:
                 break
         elif answer == 'investigate' or answer == 'iv':
-            print(gt.options[investigate])
+            investigate()
         elif answer == 'help' or answer == 'h':
             print("Options: option (shortcut)")
             for option in gt.options:
@@ -36,14 +38,6 @@ def disregard():
     print(gt.options["disregard (d)"])
 
 
-def listen():
-    print(gt.options["listen (l)"])
-
-
-def follow():
-    print(gt.options["follow (f)"])
-
-
 def exit_game(input, if_yes, if_no):
     input = input("Do you want to exit? (y/n)\n>").lower()
     if input == "y":
@@ -54,9 +48,31 @@ def exit_game(input, if_yes, if_no):
         return False
 
 
+def follow():
+    print(gt.options["follow (f)"])
+
+
+def investigate():
+    print(gt.options["investigate (iv)"])
+
 
 def game_over(state):
     print(state)
+
+
+def listen():
+    print(gt.options["listen (l)"])
+
+
+def sleep(input, if_yes, if_no):
+    print(gt.options["sleep (s)"])
+    input = input("would you like to restart?(y/n)\n>").lower()
+    if input == "y":
+        print(if_yes)
+        return not True
+    else:
+        print(if_no)
+        return not False
 
 
 start()
--- items/game_text.py
+++ items/game_text.py
@@ -1,15 +1,13 @@
-intro_message = '''It's a cold windy night with no clouds. You walk into a building called The Howling Serpent Inn.   
+intro_message = '''\tIt's a cold windy night with no clouds. You walk into a building called The Howling Serpent Inn.   
 You sit down and the bartender hands you the normal order, a fine Elven brew called Evermead.                         
-You go to sit at a table when you hear a small group of three trying to keep their voices lower than everyone else.'''
-
-intro_q = '''Why are they keeping so quiet? I could try to Listen in on their conversation,but it might be easier to just Disregard them'''
+You go to sit at a table when you hear a small group of three trying to keep their voices lower than everyone else.
+\nWhy are they keeping so quiet? You could try to Listen in on their conversation,but it might be easier to just Disregard them'''
 
 options = {
-'disregard (d)': '''You go to an isolated table in the corner to sit back, relax, and enjoy the beautiful sound of the 
-    instrument played by the bards on the stage. Out of the corner of your eye you see the small group stand up 
-    and walk out.\n\tIt looked like they were in a rush of some sort. Are they going to try to find the person 
-    responsible?  I could follow them without them even knowing, but it is getting late in the night and i am a 
-    little tired.''',
+'disregard (d)': '''\tYou go to an isolated table in the corner to sit back, relax, and enjoy the beautiful sound of the 
+instruments played by the bards on the stage. Out of the corner of your eye you see the small group stand up 
+and walk out.\n\nIt looked like they were in a rush of some sort. You could follow them without them knowing, but 
+it is getting late in the night and you do feel a little tired.''',
 
 'sleep (s)': '''As the night goes on you contiue drinking that delicious Evermead. You rent a room upstairs with a nice view of the street. 
 You undress and lay down on the bed and almost immediatly fall asleep.
@@ -25,5 +23,7 @@ The Grays? Who would be digging up dead bodies? Maybe some grave robbers? But th
 
 'investigate (iv)': f"You look around the room and see the walls and floor. It's pretty basic.",
 
-'follow (f)': '''You pull up your bootstraps and start tailing the individual you seek.'''
+'follow (f)': '''You pull up your bootstraps and start tailing the individual you seek.''',
+    
+'exit (e)': '''Are you sure you want to ext?'''
 }
