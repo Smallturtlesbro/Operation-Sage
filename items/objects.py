7fb1ea6761c2c829707686739f3a873df89cf156
Smallturtles <topher.parrish@gmail.com>
2021-09-24 21:19:54 -0700

    Merged Weapons.py into objects.py

--- items/objects.py
+++ items/objects.py
@@ -1,9 +1,65 @@
-class HayBed:
-    def __init__(self):
-        self.name = "Hay Bed"
-        self.description = "A bed of  hay with a thin cloth as a blanket. It doesnt look to comfortable to sleep on."
 
-class Mirror:
+class Weapon:
+    def __str__(self):
+        return self.name
+
+
+class Consumable:
+    def __str__(self):
+        return self.name
+
+
+class Furniture:
+    def __str__(self):
+        return self.name
+
+#Weapon
+class Dagger:
     def __init__(self):
-        self.name = "Broken Mirror"
-        self.description = "A mirror cracked and chipped in several places. I wonder if this could be used as a weapon?"
+        self.name = "Dagger"
+        self.description = "A small dagger with some rust."
+        self.damage = 10
+
+
+class Iron_bar(Weapon):
+    def __init__(self):
+        self.name = "Iron bar"
+        self.description = "A one foot iron bar, Suitable for bludgeoning."
+        self.damage = 5
+
+
+class Sword:
+    def __init__(self):
+        self.name = "Sword"
+        self.description = "A short sword."
+        self.damage = 12
+
+
+class Rock:
+    def __init__(self):
+        self.name = "Rock"
+        self.description = "A fist sized rock, could be used for bludgeoning"
+        self.damage = 5
+
+
+class Unarmed:
+    def __init__(self):
+        self.name = "Unarmed"
+        self.description = "Just some trusty ol fists"
+        self.damage = 3
+
+
+#Furniture
+class Bucket(Furniture):
+    def __init__(self):
+        self.name = "Bucket"
+        self.description = "A small wooden bucket; your only source of relief."
+
+
+#Consumable
+class Bread(Consumable):
+    def __init__(self):
+        self.name = "Bread"
+        self.description = "A stale piece of bread."
+
+
