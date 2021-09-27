6b3b2d5f39f511c77de08ce7ec54d0a4e86d2f8b
Smallturtles <topher.parrish582@gmail.com>
2021-09-27 12:34:57 -0700

    added player class with inventory and ways to move (N, S, E, W)

--- characters.py
+++ characters.py
@@ -1,6 +1,30 @@
+import items.objects as obj
+
 class Player:
-    def __str__(self):
-        return self.name
+    def __init__(self):
+        self.inventory = [obj.Bread()
+                          ]
+        self.x = 1
+        self.y = 2
+    def move(self,dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def move_north(self):
+        self.move(dx=0, dy=-1)
+
+    def move_south(self):
+        self.move(dx=0, dy=1)
+
+    def move_east(self):
+        self.move(dx=1, dy=0)
+
+    def move_west(self):
+        self.move(dx=-1, dy=0)
+
+
+
+
 
 class NPC:
     def __str__(self):
